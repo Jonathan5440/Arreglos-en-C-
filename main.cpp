f4074b4859cdbb3172c3d6bd69c23b83ccc244b0
Jonathan5440 <jvaldezd4@miumg.edu.gt>
2025-04-05 10:43:04 -0600

    funcion ejecutar programa

--- main.cpp
+++ main.cpp
@@ -14,3 +14,32 @@ int main() {
     ejecutarPrograma();  // Llamamos a la funcion ejecutarPrograma
     return 0;
 }
+void ejecutarPrograma() {
+    string tareas[MAX_TAREAS];
+    int cantidad = 0;
+    int opcion;
+
+    do {
+        mostrarMenu();
+        cin >> opcion;
+        cin.ignore(); // Limpiar
+
+        switch (opcion) {
+            case 1:
+                agregarTarea(tareas, cantidad);
+            break;
+            case 2:
+                verTareas(tareas, cantidad);
+            break;
+            case 3:
+                eliminarTarea(tareas, cantidad);
+            break;
+            case 4:
+                cout << "Saliendo del programa..." << endl;
+            break;
+            default:
+                cout << "Opcion invalida. Intente de nuevo." << endl;
+        }
+
+    } while (opcion != 4);
+}
